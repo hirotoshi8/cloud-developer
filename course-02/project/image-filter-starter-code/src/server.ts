588c07b9116bd98eaff4ef3960d97e33e6c9e813
hirotoshi8 <hirotoshi8@users.noreply.github.com>
2020-05-13 18:32:19 +0900

    Add Request and Response as Type

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-//import {Request, Response } from 'express';
+import {Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 //import { url } from 'inspector';
